dc99f29e9a77df9b471ea388828bcbefcb183477
Leonardo Campos <leonardoscampos@gmail.com>
2017-04-18 19:50:09 -0300

    webpack: banner plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,23 @@
 const path = require('path');
-const outputPath = path.resolve(__dirname, 'dist');
+const pkg = require('./package');
+const webpack = require('webpack');
 const context = path.resolve(__dirname, "src");
+const outputPath = path.resolve(__dirname, 'dist');
+
+function getCurrentDate() {
+  var today = new Date();
+  var year = today.getFullYear();
+  var month = ('00' + today.getMonth() + 1).slice(-2);
+  var date = ('00' + today.getDate()).slice(-2);
+
+  return `${year}-${month}-${date}`;
+}
+
+function getBanner() {
+  return `/*! ${pkg.name} - ${pkg.version} - ` +
+         `${getCurrentDate()} ` +
+         `| (c) 2016 Chris Hafey | https://github.com/chafey/cornerstoneWADOImageLoader */`
+}
 
 module.exports = {
   context: context,
@@ -36,5 +53,12 @@ module.exports = {
         loader: 'babel-loader'
       }]
     }]
-  }
+  },
+  plugins:[
+    new webpack.BannerPlugin({
+      banner: getBanner(),
+      entryOnly: true,
+      raw: true
+    })
+  ]
 };
